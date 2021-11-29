17f287dd75f66f3eb4dbc3c16ff1326971cb66d7
Kelsey Taylor <kelsey.e.taylor@me.com>
2021-11-28 20:47:01 -0500

    scrollytelling with glaciers

--- scrollytelling/glaciers/config.js
+++ scrollytelling/glaciers/config.js
@@ -8,6 +8,8 @@ var config = {
   chapters: [
     {
       id: "park-1850",
+      image: "GLAC.svg",
+      description: "Glaciers in Glacier National Park in 1850",
       location: {
         center: [-114.0944, 48.6343],
         zoom: 8.47,
@@ -23,7 +25,35 @@ var config = {
       onChapterExit: []
     },
     {
+      id: "blackfoot-1850",
+      description: "1850",
+      location: {
+        center: [-113.69731, 48.6014],
+        zoom: 11,
+        pitch: 0,
+        bearing: 0,
+        speed: 0.7
+      },
+      onChapterEnter: [
+        {
+          layer: "1850-glaciers",
+          opacity: 1.0
+        },
+        {
+          layer: "glacier-labels",
+          opacity: 1.0
+        }
+      ],
+      onChapterExit: [
+        {
+          layer: "1850-glaciers",
+          opacity: 0
+        }
+      ]
+    },
+    {
       id: "blackfoot-1850-1966",
+      description: "1966",
       location: {
         center: [-113.69731, 48.6014],
         zoom: 11,
@@ -58,6 +88,7 @@ var config = {
     },
     {
       id: "blackfoot-1966-1998",
+      description: "1998",
       location: {
         center: [-113.69731, 48.6014],
         zoom: 11,
@@ -96,6 +127,7 @@ var config = {
     },
     {
       id: "blackfoot-1998-2005",
+      description: "2005",
       location: {
         center: [-113.69731, 48.6014],
         zoom: 11,
@@ -142,6 +174,7 @@ var config = {
     },
     {
       id: "blackfoot-2005-2015",
+      description: "2015",
       location: {
         center: [-113.69731, 48.6014],
         zoom: 11,
@@ -195,7 +228,35 @@ var config = {
       ]
     },
     {
+      id: "agassiz-1850",
+      description: "1850",
+      location: {
+        center: [-114.1083, 48.9259],
+        zoom: 11,
+        pitch: 0,
+        bearing: 0,
+        speed: 0.7
+      },
+      onChapterEnter: [
+        {
+          layer: "1850-glaciers",
+          opacity: 1.0
+        },
+        {
+          layer: "glacier-labels",
+          opacity: 1.0
+        }
+      ],
+      onChapterExit: [
+        {
+          layer: "1850-glaciers",
+          opacity: 0
+        }
+      ]
+    },
+    {
       id: "agassiz-1850-1966",
+      description: "1966",
       location: {
         center: [-114.1083, 48.9259],
         zoom: 11,
@@ -231,6 +292,7 @@ var config = {
     },
     {
       id: "agassiz-1966-1998",
+      description: "1998",
       location: {
         center: [-114.1083, 48.9259],
         zoom: 11,
@@ -269,6 +331,7 @@ var config = {
     },
     {
       id: "agassiz-1998-2005",
+      description: "2005",
       location: {
         center: [-114.1083, 48.9259],
         zoom: 11,
@@ -315,6 +378,7 @@ var config = {
     },
     {
       id: "agassiz-2005-2015",
+      description: "2015",
       location: {
         center: [-114.1083, 48.9259],
         zoom: 11,
@@ -368,7 +432,36 @@ var config = {
       ]
     },
     {
+      id: "oldsun-1850",
+      title: "Old Sun",
+      description: "1850",
+      location: {
+        center: [-113.8116, 48.8921],
+        zoom: 11,
+        pitch: 0,
+        bearing: 0,
+        speed: 0.7
+      },
+      onChapterEnter: [
+        {
+          layer: "1850-glaciers",
+          opacity: 1.0
+        },
+        {
+          layer: "glacier-labels",
+          opacity: 1.0
+        }
+      ],
+      onChapterExit: [
+        {
+          layer: "1850-glaciers",
+          opacity: 0
+        }
+      ]
+    },
+    {
       id: "oldsun-1850-1966",
+      description: "1966",
       location: {
         center: [-113.8116, 48.8921],
         zoom: 11,
@@ -404,6 +497,7 @@ var config = {
     },
     {
       id: "oldsun-1966-1998",
+      description: "1998",
       location: {
         center: [-113.8116, 48.8921],
         zoom: 11,
@@ -442,6 +536,7 @@ var config = {
     },
     {
       id: "oldsun-1998-2005",
+      description: "2005",
       location: {
         center: [-113.8116, 48.8921],
         zoom: 11,
@@ -488,6 +583,7 @@ var config = {
     },
     {
       id: "oldsun-2005-2015",
+      description: "2015",
       location: {
         center: [-113.8116, 48.8921],
         zoom: 11,
@@ -541,7 +637,36 @@ var config = {
       ]
     },
     {
+      id: "grinnell-1850",
+      title: "Grinnell",
+      description: "1850",
+        location: {
+          center: [-113.7023, 48.7455],
+          zoom: 11,
+          pitch: 0,
+          bearing: 0,
+          speed: 0.7
+      },
+      onChapterEnter: [
+        {
+          layer: "1850-glaciers",
+          opacity: 1.0
+        },
+        {
+          layer: "glacier-labels",
+          opacity: 1.0
+        }
+      ],
+      onChapterExit: [
+        {
+          layer: "1850-glaciers",
+          opacity: 0
+        }
+      ]
+    },
+    {
       id: "grinnell-1850-1966",
+      description: "1966",
       location: {
         center: [-113.7023, 48.7455],
         zoom: 11,
@@ -577,6 +702,7 @@ var config = {
     },
     {
       id: "grinnell-1966-1998",
+      description: "1998",
       location: {
         center: [-113.7023, 48.7455],
         zoom: 11,
@@ -615,6 +741,7 @@ var config = {
     },
     {
       id: "grinnell-1998-2005",
+      description: "2005",
       location: {
         center: [-113.7023, 48.7455],
         zoom: 11,
@@ -661,6 +788,7 @@ var config = {
     },
     {
       id: "grinnell-2005-2015",
+      description: "2015",
       location: {
         center: [-113.7023, 48.7455],
         zoom: 11,
@@ -755,7 +883,9 @@ var config = {
       onChapterExit: []
     },
     {
-      id: "fires-1920",
+      id: "all-1850-2015",
+      description: "All glaciers, 1850-2015",
+      image: "GLAC.svg",
       location: {
         center: [-114.0944, 48.6343],
         zoom: 8.47,
@@ -765,19 +895,23 @@ var config = {
       },
       onChapterEnter: [
         {
-          layer: "1910-fires",
+          layer: "1850-glaciers-gone",
           opacity: 0.5
+        },
+        {
+          layer: "1966-glaciers-gone",
+          opacity: 0.5
+        },
+        {
+          layer: "1998-glaciers-gone",
+          opacity: 0.5
+        },
+        {
+          layer: "2015-glaciers",
+          opacity: 1.0
         }
       ],
       onChapterExit: [
-        {
-          layer: "1910-fires",
-          opacity: 0
-        },
-        {
-          layer: "1920-fires",
-          opacity: 0.1
-        }
       ]
     }
   ]
